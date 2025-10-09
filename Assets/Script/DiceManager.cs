3a9ea01ae8d73ee52900fdb115ac8aabe1c7f5d5
CongBinh24 <93370591+CongBinh24@users.noreply.github.com>
2025-10-10 00:29:22 +0700

    Dice
    Script DiceManager (RollDice)

---
+++ Assets/Script/DiceManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceManager : MonoBehaviour
+{
+    public List<Image> dices;// DS SpriteRenderer của xúc xắc
+    [SerializeField] private Sprite[] spr; //DS 6 mặt xúc xắc
+    private int[] diceValues;
+    void Start()
+    {
+        diceValues = new int[dices.Count];
+        StartCoroutine(AutoRoll());
+    }
+
+    //public int[] GetDiceValues()
+    //{
+    //    return diceValues;
+    //}
+
+
+    private IEnumerator AutoRoll()
+    {
+        while (true) // lặp vĩnh viễn
+        {
+            yield return StartCoroutine(PlayRound()); // Gọi vòng roll
+            yield return new WaitForSeconds(1f); // nghỉ 1 giây rồi roll tiếp
+        }
+    }
+
+    private IEnumerator PlayRound()
+    {
+        yield return new WaitForSeconds(3f);
+        for (int i = 0; i < dices.Count; i++)
+        {
+            int random = Random.Range(1, spr.Length);
+            dices[i].sprite = spr[random];
+            diceValues[i] = random;
+        }
+        Sum();
+    }
+
+    private void Sum()
+    {
+        int total = diceValues[0] + diceValues[1] + diceValues[2];
+        Debug.Log("Ket qua = " + total);
+    }
+}
